2feda21c8ed1d189db63c3fe602635e797183356
Kirill <kirillgel1os@gmail.com>
2016-08-22 23:59:50 +0200

    new slide

--- presentation.js
+++ presentation.js
@@ -1071,6 +1071,14 @@ export default () =>
       </Slide>
 
       <Slide>
+        <h2>Making React to work faster.</h2>
+        <div>2. Usage of shouldComponentUpdate().</div>
+        <PleaseGuy />
+        <div style={{marginTop: '175px'}}>Use <b>immutable</b> data!</div>
+        <div>It'll help you to perform quick equality checks and boost performance.</div>
+      </Slide>
+
+      <Slide>
         <h2>Two columns if you like</h2>
         <p className="double">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia.</p>
       </Slide>
